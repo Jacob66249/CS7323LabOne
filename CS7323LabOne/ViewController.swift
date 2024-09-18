8767d86d9437b699141ee4c6f67abc71e97300c1
Jacob66249 <394039547@qq.com>
2024-09-18 16:15:21 -0500

    Initial Commit

---
+++ CS7323LabOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS7323LabOne
+//
+//  Created by Zhiwei Cheng on 9/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
